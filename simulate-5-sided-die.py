8dd7f0aef29794fa7063ad9466ddac6e8a89a9f0
Rohan Aurora <rohanaurora@gmail.com>
2020-05-26 23:04:35 -0700

    Add source in comments.

--- simulate-5-sided-die.py
+++ simulate-5-sided-die.py
@@ -1,5 +1,6 @@
 # You have a function rand7() that generates a random integer from 1 to 7.
 # Use it to write a function rand5() that generates a random integer from 1 to 5.
+# Source - https://www.interviewcake.com/question/python/simulate-5-sided-die?
 
 import random
 
