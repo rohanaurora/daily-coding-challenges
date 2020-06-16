2ef351b39ccdea05d653015299b02b5144aff063
Rohan Aurora <rohanaurora@gmail.com>
2020-06-15 19:38:48 -0700

    Reverse string using recursion in-place and 2-pointers.

---
+++ reverse_string.py
@@ -0,0 +1,34 @@
+# Write a function that reverses a string. The input string is given as an array of characters char[].
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1)
+# extra memory. You may assume all the characters consist of printable ascii characters.
+
+# Input: ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Source - https://leetcode.com/articles/reverse-string/
+
+# Recursion
+# Space complexity : O(N) to keep the recursion stack.
+# Time complexity : O(N) time to perform N/2 swaps.
+
+class Solution:
+    def reverseString(self, s):
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
+        print(s)
+
+
+input = ["h", "e", "l", "l", "o"]
+s = Solution().reverseString(input)
+
+
+# 2 pointers solution, swap from left and right. left = right for middle
+# class Solution:
+#     def reverseString(self, s):
+#         left, right = 0, len(s) - 1
+#         while left < right:
+#             s[left], s[right] = s[right], s[left]
+#             left, right = left + 1, right - 1
