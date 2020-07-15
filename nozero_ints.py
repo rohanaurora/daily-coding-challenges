11d4b3cf3e1a2e666a52bf36926c059d3c36835f
Rohan Aurora <rohanaurora@gmail.com>
2020-07-14 22:58:34 -0700

    Add recursive method.

--- nozero_ints.py
+++ nozero_ints.py
@@ -11,3 +11,16 @@ class Solution:
 
 s = Solution().getNoZeroIntegers(11)
 print(s)
+
+
+# class Solution:
+#     def getNoZeroIntegers(self, n: int) -> List[int]:
+#         def recursive(n, count=1):
+#             if '0' not in str(n) and '0' not in str(count):
+#                 return n
+#             n -= 1
+#             count += 1
+#             return recursive(n, count)
+#
+#         num = recursive(n - 1)
+#         return [n - num, num]
