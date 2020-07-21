22349fc5f12f813bf64fa5ac786fa3067cc9084d
Rohan Aurora <rohanaurora@gmail.com>
2020-07-20 23:59:49 -0700

    f

--- sort_bits.py
+++ sort_bits.py
@@ -4,6 +4,9 @@
 #
 # Return the sorted array.
 
+# Input: arr = [0,1,2,3,4,5,6,7,8]
+# Output: [0,1,2,4,8,3,5,6,7]
+
 # class Solution:
 #     def sortByBits(self, arr):
 #         res = sorted(arr, key=lambda x: (bin(x).count('1'), x))
