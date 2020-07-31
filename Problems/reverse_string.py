9f3cb6f3a202af9401fa8a273c2680cf39a45c6b
Rohan Aurora <rohanaurora@gmail.com>
2020-07-30 23:44:45 -0700

    Add one-liner to reverse the string.

--- Problems/reverse_string.py
+++ Problems/reverse_string.py
@@ -32,3 +32,7 @@ s = Solution().reverseString(input)
 #         while left < right:
 #             s[left], s[right] = s[right], s[left]
 #             left, right = left + 1, right - 1
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         for i in range(len(s)//2):s[i],s[-i-1]=s[-i-1],s[i]
