bb3da2298062181724b4271fdeefe508a2696d81
Rohan Aurora <rohanaurora@gmail.com>
2020-08-06 23:56:11 -0700

    Array using Stack Ops.

--- build_array.py
+++ build_array.py
@@ -1,3 +1,17 @@
+# Given an array target and an integer n. In each iteration, you will read a number from  list = {1,2,3..., n}.
+
+# Build the target array using the following operations:
+
+# Push: Read a new element from the beginning list, and push it in the array.
+# Pop: delete the last element of the array.
+# If the target array is already built, stop reading more elements.
+# You are guaranteed that the target array is strictly increasing, only containing numbers between 1 to n inclusive.
+
+# Return the operations to build the target array.
+
+# You are guaranteed that the answer is unique.
+# Source - https://leetcode.com/problems/build-an-array-with-stack-operations/
+
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
 
