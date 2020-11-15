8cc285d598c6bd4d04a590dd9563a3f302261c29
Rohan Aurora <rohanaurora@gmail.com>
2020-11-14 23:21:55 -0800

    Add O(n) solution.

--- ShuffleStrings.playground/Contents.swift
+++ ShuffleStrings.playground/Contents.swift
@@ -1,3 +1,26 @@
 import Cocoa
 
-var str = "Hello, playground"
+//Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+//Output: "leetcode"
+//Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+
+class Solution {
+    func restoreString(_ s: String, _ indices: [Int]) -> String {
+        
+        var output = Array(s)
+        let s = Array(s)
+        
+        for (index, value) in indices.enumerated() {
+            output[value] = s[index]
+        }
+        
+        return String(output)
+    }
+}
+
+let s = "codeleet"
+let i = [4,5,6,7,0,2,1,3]
+let sol = Solution()
+print(sol.restoreString(s, i))
+
